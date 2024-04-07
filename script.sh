6e9eaea015dd75c5a5876432a6507c697fc32161
ViktorKonovalenko <32430041+ViktorKonovalenko@users.noreply.github.com>
2024-04-07 11:14:45 +0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -3,7 +3,6 @@ echo -e "PID\tTTY\tSTAT\tTIME\tCOMMAND"
 pid_array=$(ls /proc/|egrep [[:digit:]]|sort -n)
 for i in ${pid_array[@]}
 do
-#echo -e -n "$i\t"
 termin='?'
 if [ -h /proc/$i/fd/0 ]
 then
@@ -16,18 +15,15 @@ then
                 fi
         fi
 fi
-#echo -e -n "$termin\t"
 if [ -f /proc/$i/status ]
 then
 procstat=$(sed -n '/State/p' /proc/$i/status |cut -d':' -f2 |cut -d'(' -f1 2>/dev/null)
 fi
-#echo -e -n "$procstat\t"
 utime=$(cut -d' ' -f14 /proc/$i/stat 2>/dev/null)
 ctime=$(cut -d' ' -f15 /proc/$i/stat 2>/dev/null)
 proctime=$((utime+ctime))
 proctime_min=$(($proctime/100/60))
 proctime_sec=$(($proctime/100%60))
-#echo -e -n "$proctime_min:$proctime_sec\t"
 if [ -d /proc/$i ]
 then
 	cmdline_length=$(wc -m /proc/$i/cmdline | cut -d' ' -f1)
@@ -38,6 +34,5 @@ then
 		proccommand=$(tr -d '\0' </proc/$i/stat | cut -d' ' -f2)
 	fi
 fi
-#echo -e "$proccommand"
 echo -e "$i\t$termin\t$procstat\t$proctime_min:$proctime_sec\t$proccommand"
 done
